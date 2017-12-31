058b7a659e4f29f9ba4621b7cb608a7600813931
Kiba Gateaux <kibagateaux@gmail.com>
2017-12-31 18:50:37 -0500

    instantiates anonId in Base

--- src/Base.js
+++ src/Base.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import Amplify from 'aws-amplify-react-native';
+import uuid from 'uuid';
 
 import {AWS_CONFIG, initAWS} from '@constants/AWS';
 
-import store from './store/';
-import Router from './router/';
+import store from './store';
+import Router from './router';
+
+import {identifyUser} from '@actions/analytics';
 
 initAWS();
 Amplify.configure(AWS_CONFIG);
+store.dispatch(identifyUser({anonymousId: uuid.v4()}));
+
 
 export default (props) => (
   <Provider store={store}>
