85e2c1331210f3d2e7c48853f8934a08c6607def
Kiba Gateaux <kibagateaux@gmail.com>
2018-01-03 15:49:37 -0500

    adds crash analytics with sentry.io

--- android/app/src/main/java/com/emochi/MainApplication.java
+++ android/app/src/main/java/com/emochi/MainApplication.java
@@ -3,6 +3,7 @@ package com.emochi;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.sentry.RNSentryPackage;
 import io.branch.rnbranch.RNBranchPackage;
 import io.branch.referral.Branch;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSentryPackage(MainApplication.this),
             new RNBranchPackage(),
             new VectorIconsPackage(),
             new RNAWSCognitoPackage()
--- src/Base.js
+++ src/Base.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import Amplify from 'aws-amplify-react-native';
-import uuid from 'uuid';
-import store from './store';
-import Router from './router';
 
+import {Sentry, SentryLog} from 'react-native-sentry';
+
+if(!__DEV__) {
+  Sentry.config(
+    'https://e9b558133e3540469095d69293955b49:2f119cfe91da423a9c82ca704caeff6c@sentry.io/266488',
+    {logLevel: SentryLog.Verbose}
+  ).install();
+}
+
+import Amplify from 'aws-amplify-react-native';
 import {AWS_CONFIG, initAWS} from '@constants/AWS';
 initAWS();
 Amplify.configure(AWS_CONFIG);
 
+import {Provider} from 'react-redux';
+import store from './store';
+import Router from './router';
 
 export default (props) => (
   <Provider store={store}>
